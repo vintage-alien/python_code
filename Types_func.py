27ebeb1d376f836271ceb136044a2774e24318a1
Md Hasanujjaman <76647218+vintage-alien@users.noreply.github.com>
2022-06-12 18:02:54 +0530

    Add files via upload

---
+++ Types_func.py
@@ -0,0 +1,16 @@
+# type() - used to get types of a variable
+var1 = "News"
+var2 = 394
+var3 = 21548796414515485121213646163769646719638632357197681671111971987
+var4 = 'a'
+var5 = 145.3
+var6 = 2458.712178458646326851681566863116831
+
+print("printing types of different variables : ")
+
+print("type of var1 :" + str(type(var1)))
+print("type of var2 :" + str(type(var2)))
+print("type of var3 :" + str(type(var3)))
+print("type of var4 :" + str(type(var4)))
+print("type of var5 :" + str(type(var5)))
+print("type of var6 :" + str(type(var6)))
---
+++ basics.py
@@ -0,0 +1,3 @@
+units=input("how many units do you want : ")
+price_per_unit=10
+print("Total price = "+ str(int(units)*price_per_unit))
